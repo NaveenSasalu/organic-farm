5705d418008a7e8e621d316b55c969ac7cff7cd5
NaveenSasalu <nkamalas@gmail.com>
2025-12-31 12:33:17 +0530

    reverting to dev code - prd dis issue fix

--- fend/src/lib/api.ts
+++ fend/src/lib/api.ts
@@ -3,21 +3,27 @@ export const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_URL || "https://of.kaayaka.in/api/v1";
 
 export async function fetchProducts() {
-  try {
-    // 1. apiRequest returns the actual DATA (e.g., an array of products)
-    // not the "Response" object.
-    const products = await apiRequest("/products", {
-      method: "GET",
-    });
-
-    // chaged to return json
-    return products.json();
-  } catch (err: any) {
-    // 2. The error thrown here comes from the 'throw' inside apiRequest
-    console.error("Fetch error:", err.message);
-    throw err; // Re-throw so the UI component can show an error state
-  }
+  const res = await fetch(`${API_BASE_URL}/products`);
+  if (!res.ok) throw new Error("Farm is resting. Try again later.");
+  return res.json();
 }
+
+// export async function fetchProducts() {
+//   try {
+//     // 1. apiRequest returns the actual DATA (e.g., an array of products)
+//     // not the "Response" object.
+//     const products = await apiRequest("/products", {
+//       method: "GET",
+//     });
+
+//     // chaged to return json
+//     return products.json();
+//   } catch (err: any) {
+//     // 2. The error thrown here comes from the 'throw' inside apiRequest
+//     console.error("Fetch error:", err.message);
+//     throw err; // Re-throw so the UI component can show an error state
+//   }
+// }
 
 export async function apiRequest(endpoint: string, options: RequestInit = {}) {
   // Ensure the endpoint starts with a slash
