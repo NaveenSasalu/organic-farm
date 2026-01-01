26e023e6829af02401bada36c1945a946d6e02f8
NaveenSasalu <nkamalas@gmail.com>
2026-01-01 16:07:46 +0530

    min and usr issue fix - 3

--- bend/app/api/v1/endpoints/upload.py
+++ bend/app/api/v1/endpoints/upload.py
@@ -6,7 +6,7 @@ router = APIRouter()
 
 s3_client = boto3.client(
     "s3",
-    endpoint_url=f"{settings.MINIO_ENDPOINT}",
+    endpoint_url=f"https://{settings.MINIO_ENDPOINT}",
     aws_access_key_id=settings.MINIO_ACCESS_KEY,
     aws_secret_access_key=settings.MINIO_SECRET_KEY,
 )
@@ -23,5 +23,5 @@ async def upload_product_image(product_id: int, file: UploadFile = File(...)):
     )
     
     # 2. Return the path (In real app, update DB here)
-    image_path = f"{settings.MINIO_ENDPOINT}/{settings.MINIO_BUCKET_NAME}/{product_id}_{file.filename}"
+    image_path = f"https://{settings.MINIO_ENDPOINT}/{settings.MINIO_BUCKET_NAME}/{product_id}_{file.filename}"
     return {"image_url": image_path}
